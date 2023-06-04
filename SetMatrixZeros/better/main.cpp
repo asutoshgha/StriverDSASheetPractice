e960681109142d2d7307f4592cdf102e5399d6ff
Asutosh Ghanto <asutosh@clevertap.com>
2023-06-04 15:12:26 +0530

    SetMatrixZeros: adding the first solution

--- SetMatrixZeros/better/main.cpp
+++ SetMatrixZeros/better/main.cpp
@@ -1,4 +1,6 @@
-/* will run on coding blocks website */
+/* will run on coding blocks website 
+   https://www.codingninjas.com/codestudio/problems/set-matrix-zeros_3846774?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
+*/
 #include <bits/stdc++.h>
 
 void setZeros(vector<vector<int>> &matrix)
